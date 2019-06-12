7c4b1ec44dd76e58010bf9365d5ff8f4cc2d32e3
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2019-06-12 19:09:50 -0300

    Improve script to filter with one request

--- python/scripts/github-issues-metrics.py
+++ python/scripts/github-issues-metrics.py
@@ -4,15 +4,18 @@ import requests
 import json
 import sys
 import os
+from functools import reduce
 
 CURRENT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 class GithubSearch(object):
     api_uri = 'https://api.github.com/search/'
     config = {}
+    all_issues = {}
 
-    def __init__(self, config):
+    def __init__(self, config, since_date):
         self.config = config
+        self.all_issues = self.getIssuesFromDate(since_date)
 
     def githubRequest(self, endpoint, payload):
         uri = self.api_uri + endpoint
@@ -29,36 +32,41 @@ class GithubSearch(object):
             'q': 'repo:{1} is:issue is:closed closed:>={0}'.format(since_date, self.config['repository_path'])
         }
         response = self.githubRequest('issues', payload)
+
+        return response.json()
+
+    def getIssuesMock(self, since_date):
+        file_path = os.path.join(CURRENT_ROOT, 'test', 'api_request_mock.json')
+        with open(file_path, "r") as read_file:
+            response = json.load(read_file)
+
         return response
 
-    def getIssuesByLabel(self, since_date, label):
-        payload = {'q': 'repo:{2} is:issue is:closed closed:>={0} label:{1}'.format(since_date, label, self.config['repository_path'])}
-        response = self.githubRequest('issues', payload)
+    def getIssuesByLabel(self, filter_label):
+        filtered_items = list(filter(
+            lambda item: filter_label in [label['name'] for label in item['labels']],
+            self.all_issues['items']
+        ))
 
-        return {'total_count': response.json()['total_count']}
+        return filtered_items
 
-    def print_pretty_json(self, response_json):
-        print(json.dumps(response_json, sort_keys=True, indent=4))
+    def pretty_json(self, response_json):
+        return json.dumps(response_json, sort_keys=True, indent=4)
 
-    def getIssuesByAssignee(self, since_date, assignee):
-        payload = {'q': 'repo:{2} is:issue is:closed closed:>={0} assignee:{1}'.format(since_date, assignee, self.config['repository_path'])}
-        response = self.githubRequest('issues', payload)
-        response = response.json()
+    def getIssuesByAssignee(self, filter_assignee):
+        filtered_items = list(filter(
+            lambda item: filter_assignee in [assignee['login'] for assignee in item['assignees']],
+            self.all_issues['items']
+        ))
 
-        pair_count = 0
-        if 'items' not in response:
-            return {
-                total_count: response['total_count'],
-                pair_count: pair_count
-            }
-
-        for item in response['items']:
-            assignees_count = len(item['assignees'])
-            if assignees_count > 1:
-                pair_count += 1
+        pair_count = reduce(
+            lambda count, n_assignees: count + (1 if n_assignees > 1 else 0),
+            [len(item['assignees']) for item in filtered_items],
+            0
+        )
 
         return {
-            'total_count': response['total_count'],
+            'items': filtered_items,
             'pair_count': pair_count
         }
 
@@ -69,37 +77,37 @@ def main():
         config = json.load(read_file)
 
     since_date=sys.argv[1]
-    githubSearch = GithubSearch(config['github'])
+    githubSearch = GithubSearch(config['github'], since_date)
 
-    response = githubSearch.getIssuesFromDate(since_date)
-    response = response.json()
-    issues_total = response['total_count']
-    print()
-    print("Total de tarefas: {0}".format(issues_total))
-    print()
+    data_result = {
+        'total_tasks': githubSearch.all_issues['total_count'],
+        'collaborators': [],
+        'labels': []
+    }
 
     for assignee in config['collaborators']:
-        response = githubSearch.getIssuesByAssignee(since_date, assignee['username'])
-
         name = assignee['name']
         if 'company' in assignee.keys():
             company = ' ({0})'.format(assignee['company'])
             name = name + company
 
-        result = "{0}: {1} total ({2} pair programming)".format(
-            name,
-            response['total_count'],
-            response['pair_count']
-        )
-        print(result)
+        response = githubSearch.getIssuesByAssignee(assignee['username'])
 
-    print()
+        data_result['collaborators'].append({
+            'name': name,
+            'tasks_count': len(response['items']),
+            'pair_count': response['pair_count']
+        })
+
 
     for label in config['labels']:
-        response = githubSearch.getIssuesByLabel(since_date, '"{0}"'.format(label))
-        print("{0}: {1}".format(label, response['total_count']))
+        items = githubSearch.getIssuesByLabel(label)
+        data_result['labels'].append({
+            'label': label,
+            'tasks_count': len(items)
+        })
 
-    print()
+    print(githubSearch.pretty_json(data_result))
 
 if __name__ == "__main__":
     main()
